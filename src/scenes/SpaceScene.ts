1b4d60965ec6ad5a9f63d5a799de0d7dbab0a83f
Flavio Carlo Ricci <flavio.ricci@doctolib.com>
2025-03-12 10:53:07 +0100

    jetpack

--- src/scenes/SpaceScene.ts
+++ src/scenes/SpaceScene.ts
@@ -3,6 +3,7 @@ import { Scene } from 'phaser';
 export class SpaceScene extends Scene {
     private astronaut!: Phaser.Physics.Arcade.Sprite;
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+    private background!: Phaser.GameObjects.Image;
 
     constructor() {
         super({ key: 'SpaceScene' });
@@ -11,32 +12,45 @@ export class SpaceScene extends Scene {
     preload() {
         // Load the astronaut sprite
         this.load.image('astronaut', 'assets/astronaut.png');
+        // Load the background image
+        this.load.image('space-bg', 'assets/space-bg.png');
     }
 
     create() {
+        // Create the background image and stretch it to cover the entire screen
+        this.background = this.add.image(0, 0, 'space-bg');
+        this.background.setOrigin(0, 0);
+        this.background.displayWidth = this.scale.width;
+        this.background.displayHeight = this.scale.height;
+
         // Create the astronaut sprite and enable physics
         this.astronaut = this.physics.add.sprite(512, 384, 'astronaut');
         this.astronaut.setCollideWorldBounds(true);
+        this.astronaut.setScale(1.5); // Increase the size by 1.5 times
 
         // Create cursor keys for controlling the astronaut
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
-        // Reset the velocity
-        this.astronaut.setVelocity(0);
-
-        // Update the astronaut's velocity based on the arrow keys
-        if (this.cursors.left.isDown) {
-            this.astronaut.setVelocityX(-200);
-        } else if (this.cursors.right.isDown) {
-            this.astronaut.setVelocityX(200);
-        }
-
+        // Apply thrust when the up arrow key is pressed
         if (this.cursors.up.isDown) {
-            this.astronaut.setVelocityY(-200);
-        } else if (this.cursors.down.isDown) {
-            this.astronaut.setVelocityY(200);
+            this.physics.velocityFromRotation(this.astronaut.rotation, 200, this.astronaut.body.acceleration);
+        } else {
+            this.astronaut.setAcceleration(0);
         }
+
+        // Rotate the astronaut based on left/right arrow keys
+        if (this.cursors.left.isDown) {
+            this.astronaut.setAngularVelocity(-300); // Increase rotation speed
+        } else if (this.cursors.right.isDown) {
+            this.astronaut.setAngularVelocity(300); // Increase rotation speed
+        } else {
+            this.astronaut.setAngularVelocity(0);
+        }
+
+        // Apply drag to simulate space friction
+        this.astronaut.setDamping(true);
+        this.astronaut.setDrag(0.99);
     }
 }
